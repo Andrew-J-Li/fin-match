e40cbe5086b102f400a7b6219122c147ea932621
Andrew Li <andrewjli@utexas.edu>
2025-01-25 00:56:21 -0600

    Add Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './styles/App.css'; // Global styles
-import Header from './components/Header';
-
+import Header from './components/Header'; // Import Header component
+import Home from './components/Home'; // Import the new Home component
 
 function App() {
   return (
     <div className="App">
       <Header /> {/* Add the Header component */}
-      <main>
-      </main>
+      <Home />   {/* Add the Home component */}
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Home.css'; // Import styles for the Home component
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main content of the home page.</p>
+    </div>
+  );
+}
+
+export default Home;
