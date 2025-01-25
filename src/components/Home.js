b06fd09628d231413181dd080751dd76be029fda
Andrew Li <andrewjli@utexas.edu>
2025-01-25 12:52:33 -0600

    Add Home Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,19 @@ import styles from '../styles/Home.css';
 import { Briefcase, Target, UserCheck, MessageSquare } from 'lucide-react';
 
 const Home = () => (
-  
-  <div className="background">
-    <div className="home-text">
-      <p>
-        Revolutionizing the way you invest by seamlessly matching prospective investors with financial advisors using AI and machine learning.
-      </p>
+  <div style={{ width: "100%", height: "100vh", overflow: "hidden", position: "relative"}}>
+    <div>
+    <img src={require('../assets/stock.png')} alt="Financial Advisor" style ={{width: "100%"}}/>
+    <div className="background">
+      <div className="home-text">
+        <p>
+          Revolutionizing the way you invest by seamlessly matching prospective investors with financial advisors using AI and machine learning.
+        </p>
+      </div>
+    </div>
     </div>
   </div>
-
+  
 );
 
 export default Home;
