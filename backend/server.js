67e63ac534366386b6ecc036e214448ada51a46b
Krish Parikh <kparikh0509@gmail.com>
2025-01-25 21:16:50 -0600

    backend setup
    Co-authored-by: Aaron Chen <aaronthechen@users.noreply.github.com>

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const app = express();
+const client = require('./db');
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Define a route to test the database connection
+app.get('/test', (req, res) => {
+  res.send('Backend is running');
+});
+
+//
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
